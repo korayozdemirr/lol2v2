c7ec7df8ee0b629ed0208cf905b1f60ee0965419
korayozdemirr <koray.ozdemir11@gmail.com>
2021-08-07 07:34:54 +0300

    işlem başarılı

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -5,19 +5,16 @@ const fetch = require("node-fetch");
 
 router.get("/", async (req, res) => {
     let users;
+    let user ;
     if (req.session.user) {
          //user değiştirilecek
-        user = firebase.auth().currentUser;
-        if (user.photoURL != null) {
-            let id =  user.photoURL;
-            fetch(`https://tr1.api.riotgames.com/lol/summoner/v4/summoners/${users.photoURL}?api_key=RGAPI-8e1951b8-e889-4c0c-87f8-1cf7d096e7f1`)
-                .then((response) => {
-                    users = response;
-                    return  response.json();
-                })
-                .then(async(user)=>{
-                    
-                    firebase.database().ref().child("loluser").child(`${id}`).set({
+        users = firebase.auth().currentUser;
+        if (users.photoURL != null) {
+            let id =  users.photoURL;
+           await fetch(`https://tr1.api.riotgames.com/lol/summoner/v4/summoners/${users.photoURL}?api_key=RGAPI-8e1951b8-e889-4c0c-87f8-1cf7d096e7f1`)
+                .then((response) => response.json())
+                .then((user)=>{
+                    firebase.database().ref().child("loluser").child(id).set({
                         accountId: user.accountId,
                         name: user.name,
                         profileIconId: user.profileIconId,
@@ -26,18 +23,17 @@ router.get("/", async (req, res) => {
                         summonerLevel: user.summonerLevel,
                         user: req.session.user
                     });
-                    return user
+                    users = user
                 })
-                .then(data =>console.log(data))
                 .catch((error) => {
                     console.error(error);
                 });
-                console.log(users);
         }
         res.render("index", { users });
-    } else {
+    }else{
         res.render("index", { users });
     }
+    
 });
 router.get("/logout", async (req, res) => {
     if (req.session.user) {
